307a9ba2978ba8fb37ca4b5a47579a920a08c733
Angel Figueroa <angelnewrg4l@outlook.com>
2022-07-19 20:15:29 -0500

    ToDoVue2App
    Agregando directorio del ToDo App en Vue 2

---
+++ ToDoAPP/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container mt-5">
+    <h3 style="display:inline-block">ToDo con Vue 2 y Vite</h3><img src="./assets/logo.png" alt="Vue-logo" width="50" height="50" class="mb-2">
+    <input type="text" autofocus placeholder="Agrega una tarea" class="form-control my-3" v-model="nuevaTarea" @keyup.enter="agregarTarea">
+    <button class="btn btn-primary" @click="agregarTarea">Agregar</button>
+    <button class="btn btn-warning m-2" @click="limpiarTodo">Limpiar todo</button>
+    <div class="alert alert-danger  d-flex justify-content-between align-items-center" role="alert" v-if="alerta">El campo está vacío, agrega texto</div>
+    <div class="alert alert-warning d-flex justify-content-between align-items-center" role="alert" v-if="tareas.length<=0">Awb no hay na que hacer</div>      
+    <Tarea v-else @editarTarea="editarTarea"  @eliminarTarea="eliminarTarea" :tareas="tareas" syle="color:#e63000"/>
+  </div>
+</template>
+
+<script>
+import Tarea from "./components/Tarea.vue";
+export default {
+  data(){
+    return{
+      nuevaTarea:"",
+      tareas:[],
+      alerta:false
+    }
+  },
+  components:{
+    Tarea
+  },
+  methods:{    
+    agregarTarea(){      
+      if(this.nuevaTarea.length<=0){this.alerta=true; return}      
+      this.tareas.push({nombre:this.nuevaTarea,estado:true})
+      console.log(this.tareas)
+      this.alerta = false;
+      this.nuevaTarea = '';
+      localStorage.setItem("datos",JSON.stringify(this.tareas))
+    },
+    editarTarea(index){
+      this.tareas[index].estado = !this.tareas[index].estado;
+      localStorage.setItem("datos",JSON.stringify(this.tareas))
+    },
+    eliminarTarea(i){
+      this.tareas.splice(i,1)
+      localStorage.setItem("datos",JSON.stringify(this.tareas))
+    },
+    limpiarTodo(){
+      this.tareas=[]
+      localStorage.setItem("datos",JSON.stringify(this.tareas))
+    }
+
+  },
+  created(){
+    let datosLS = JSON.parse(localStorage.getItem("datos")) 
+    if(datosLS===null) this.tareas = []
+    else this.tareas=datosLS  
+    console.log("tareas",this.tareas)
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ToDoAPP/src/components/Tarea.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+  <div class="mt-3" v-for="(item,index) of tareas" :key="index">    
+      <div  :class="{'alert-secondary': !item.estado,'alert-success':item.estado }" class="alert d-flex justify-content-between align-items-center" role="alert">
+       <div class="tituloTarea" :style="!item.estado?'text-decoration: line-through;':''">{{index}}. {{item.nombre}}</div>       
+       <div class="contenedor-botones">
+        <button class="btn btn-success m-2" @click="editarTarea(index)">OKss</button>
+        <button class="btn btn-danger" v-on:click="eliminarTarea(index)">X</button>
+       </div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        tareas:Array
+    //props:['tareas']
+    },
+    methods: {
+        editarTarea(id){
+            this.$emit('editarTarea',id)
+        },
+        eliminarTarea(id){
+            this.$emit('eliminarTarea',id)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
