a37a74c40f82a1979b67895be8bd069ff1eebf21
Angel Figueroa <angelnewrg4l@outlook.com>
2022-07-24 21:50:20 -0500

    Vuex con vue 2
    vuex con vue 2 y mapeadores

--- vue2-cli-vuex2/src/App.vue
+++ vue2-cli-vuex2/src/App.vue
@@ -1,22 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">    
+    <h1>Pruebas con Vuex 3</h1>
+    <div style="width:90%; border:1px solid lightgray; margin:0 auto;">
+      <h5>Numero desde el store normal: {{getNumeroOperacion}}</h5>
+      <h5>Numero desde el store normal: {{numero}}</h5>
+      <h5>Numero desde el store normal: {{prueba}}</h5>
+      <h5>Numero desde el store normal: {{this.$store.state.numero}}</h5>
+      <h5>Numero desde el store normal: {{this.$store.state.numero}}</h5>
+      <h5>Numero desde el store normal: {{this.$store.state.numero}}</h5>
+      <div v-if="pokemones.length>0">
+        <ul v-for="pokemon of pokemones" :key="pokemon.name">
+          <li>{{pokemon.name}}</li>
+        </ul>
+      </div>
+      <div v-else> <h5>CARGANDO POKEMONES.....</h5></div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {mapState,mapActions,mapMutations, mapGetters} from 'vuex'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  created(){
+    console.log("desde el created",this.pokemones)
+    this.obtenerPokemones()
+  },
+  computed:{
+    ...mapState(['numero','pokemones']),
+    ...mapGetters(['getNumeroOperacion']),
+    prueba(){
+      return this.numero + 10000
+    }    
+  },
+  methods:{
+    ...mapMutations(['aumentar']),
+    ...mapActions(['obtenerPokemones'])
   }
 }
 </script>
 
 <style>
+body{
+  background: #2c3e50;
+}
+h5,h1,li{
+  color:white;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- vue2-cli-vuex2/src/store/index.js
+++ vue2-cli-vuex2/src/store/index.js
@@ -5,13 +5,53 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    numero:0,
+    pokemones: []
   },
-  getters: {
-  },
+  //se usan para mutar o modificar el estado y se llama con commit('nombreMutation')
   mutations: {
+    //recibe el estado como primer argumento
+    aumentar(state){
+      state.numero ++
+    },
+    SetPokemones(state,payload){
+      state.pokemones = payload
+    }
   },
-  actions: {
+  //se usan para poner logica, pueden ser asincronas y lo ideal es llamar a las mutaciones desde aquí
+  //las actions se llaman con dispatch('nombreAction')
+  actions:{
+    async obtenerPokemones({commit}){
+      try {
+        const pokemonesAsync = await (await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0')).json()
+        console.log(pokemonesAsync)
+        commit('SetPokemones',pokemonesAsync.results)
+
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
   },
-  modules: {
+  //se usan para llamar una parte del estado, ideales para solo llamar un pedazo de todo el objeto store
+  //idealmente se deben llamar en las propiedades computed de los componentes con
+  //You can think of them as computed properties for stores.
+  //se pueden utilizar helpers para getters con ...mapGetters(['getNumero','getNumeroOperacion'])
+  getters:{
+    //recibe el estado como primer argumento    
+    getNumero( state ){
+      return (state.numero * 2) -2
+    },
+    //como segundo argumento pueden recibir el mismo objeto getters para mandar a llamar algún otro que se necesite
+    getNumeroOperacion(state,getters){
+      return state.numero + getters.getNumero
+    }
+
   }
+  
 })
+
+/*
+  modules: {
+  } */
