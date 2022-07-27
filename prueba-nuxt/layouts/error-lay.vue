155cab8b78bdd33dd06e00208f30af93d9a52465
Angel Figueroa <angelnewrg4l@outlook.com>
2022-07-26 21:22:46 -0500

    indicaciones para nuxt 2

---
+++ prueba-nuxt/layouts/error-lay.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <h1 style="color:red;">este es el layout de error</h1>
+    <Nuxt/>
+
+</div>
+</template>
---
+++ prueba-nuxt/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div><h1>este es el index</h1></div>
+</template>
+
+<script>
+export default {
+  layout:'error-lay',
+  name: 'IndexPage'
+}
+</script>
